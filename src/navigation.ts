72942ed056ca60e2aa11f5609991bbc4d0c23f03
Ender Jonel Guevara Narea <iddeia.global@gmail.com>
2025-07-27 03:22:49 +0000

    aplica la misma logica de la opcion home del menu lateral a las demas op

--- src/navigation.ts
+++ src/navigation.ts
@@ -9,18 +9,73 @@ import { locales } from './middleware';
 // from your `i18n.ts` file.
 export const pathnames = {
   '/': '/',
-  '/servicios': '/servicios',
-  '/sobre-nosotros': '/sobre-nosotros',
+  '/servicios': {
+    pt: '/servicos',
+    es: '/servicios',
+    en: '/services',
+    fr: '/services'
+  },
+  '/sobre-nosotros': {
+    pt: '/sobre-nos',
+    es: '/sobre-nosotros',
+    en: '/about-us',
+    fr: '/a-propos'
+  },
   '/blog': '/blog',
-  '/contacto': '/contacto',
-  '/backoffice': '/backoffice',
-  '/backoffice/inicio': '/backoffice/inicio',
-  '/backoffice/servicios': '/backoffice/servicios',
-  '/backoffice/sobre-nosotros': '/backoffice/sobre-nosotros',
-  '/backoffice/blog': '/backoffice/blog',
-  '/backoffice/contacto': '/backoffice/contacto',
-  '/backoffice/footer': '/backoffice/footer',
-  '/backoffice/configuracoes': '/backoffice/configuracoes'
+  '/contacto': {
+    pt: '/contacto',
+    es: '/contacto',
+    en: '/contact',
+    fr: '/contact'
+  },
+  '/backoffice': {
+    pt: '/backoffice',
+    es: '/backoffice',
+    en: '/backoffice',
+    fr: '/backoffice'
+  },
+  '/backoffice/inicio': {
+    pt: '/backoffice/inicio',
+    es: '/backoffice/inicio',
+    en: '/backoffice/home',
+    fr: '/backoffice/accueil'
+  },
+  '/backoffice/servicos': {
+    pt: '/backoffice/servicos',
+    es: '/backoffice/servicios',
+    en: '/backoffice/services',
+    fr: '/backoffice/services'
+  },
+  '/backoffice/sobre-nosotros': {
+    pt: '/backoffice/sobre-nos',
+    es: '/backoffice/sobre-nosotros',
+    en: '/backoffice/about-us',
+    fr: '/backoffice/a-propos'
+  },
+  '/backoffice/blog': {
+    pt: '/backoffice/blog',
+    es: '/backoffice/blog',
+    en: '/backoffice/blog',
+    fr: '/backoffice/blog'
+  },
+  '/backoffice/contacto': {
+    pt: '/backoffice/contacto',
+    es: '/backoffice/contacto',
+    en: '/backoffice/contact',
+    fr: '/backoffice/contact'
+  },
+  '/backoffice/footer': {
+    pt: '/backoffice/rodape',
+    es: '/backoffice/footer',
+    en: '/backoffice/footer',
+    fr: '/backoffice/pied-de-page'
+  },
+  '/backoffice/configuracoes': {
+    pt: '/backoffice/configuracoes',
+    es: '/backoffice/configuraciones',
+    en: '/backoffice/settings',
+    fr: '/backoffice/parametres'
+  }
 } satisfies Pathnames<typeof locales>;
 
 export const { Link, redirect, usePathname, useRouter } =

d945dcd090bd68ee86415fa8386d93bd8ffeb7ab
Ender Jonel Guevara Narea <iddeia.global@gmail.com>
2025-07-27 02:35:26 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/i18n.ts
+++ src/i18n.ts
@@ -1,5 +1,11 @@
-import { getRequestConfig } from 'next-intl/server';
+import {notFound} from 'next/navigation';
+import {getRequestConfig} from 'next-intl/server';
+import { locales } from './middleware';
 
-export default getRequestConfig(async ({ locale }) => ({
+export default getRequestConfig(async ({locale}) => {
+  if (!locales.includes(locale as any)) notFound();
+
+  return {
     messages: (await import(`../messages/${locale}.json`)).default
-}));
+  };
+});
--- src/middleware.ts
+++ src/middleware.ts
@@ -1,10 +1,13 @@
 import createMiddleware from 'next-intl/middleware';
 
+export const locales = ['pt', 'es', 'en', 'fr'];
+export const defaultLocale = 'pt';
+
 export default createMiddleware({
-  locales: ['pt', 'es', 'en', 'fr'],
-  defaultLocale: 'pt'
+  locales,
+  defaultLocale,
 });
 
 export const config = {
-  matcher: ['/((?!api|_next|_vercel|.*\\..*).*)']
+  matcher: ['/((?!api|_next/static|_next/image|favicon.ico).*)']
 };
